b806495d3e1bff1126d774ee96fbf95a6b14bd91
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-08-01 12:50:01 -0400

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -7,31 +7,31 @@
  */
 
 
-//define parameters
-//$host = "localhost";
-//$login = "phpuser";
-//$password = "phpuser";
-//$database = "shoes";
-//$tableShoes = "shoes";
-//$tableSizes = "sizes";
-//$tableTypes = "types";
-//
-//
-////connect to the mysql server
-//$conn = @new mysqli($host, $login, $password, $database);
-//
-//if ($conn->connect_errno) {
-//    $error = $conn->connect_error;
-//    header("Location: error.php?m=$error");
-//    die();
-//}
+define parameters
+$host = "localhost";
+$login = "phpuser";
+$password = "phpuser";
+$database = "***";
+$tableShoes = "***";
+$tableSizes = "***";
+$tableTypes = "***";
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+//connect to the mysql server
+$conn = @new mysqli($host, $login, $password, $database);
+
+if ($conn->connect_errno) {
+   $error = $conn->connect_error;
+   header("Location: error.php?m=$error");
+   die();
+}
+
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+// // Connect to DB
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
